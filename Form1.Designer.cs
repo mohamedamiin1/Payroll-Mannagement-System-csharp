eddba82b93c6da33dc19219e906472c57540f160
mohamedamiin1 <91801384+mohamedamiin1@users.noreply.github.com>
2022-11-07 20:51:19 -1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PayrollSytem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsEmployee = new System.Windows.Forms.ToolStripButton();
+            this.tsPayroll = new System.Windows.Forms.ToolStripButton();
+            this.tsUser = new System.Windows.Forms.ToolStripButton();
+            this.tsReport = new System.Windows.Forms.ToolStripButton();
+            this.tsLogin = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(50, 34);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsEmployee,
+            this.tsPayroll,
+            this.tsUser,
+            this.tsReport,
+            this.tsLogin});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(69, 505);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsEmployee
+            // 
+            this.tsEmployee.Enabled = false;
+            this.tsEmployee.Image = global::PayrollSytem.Properties.Resources.empgroup;
+            this.tsEmployee.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsEmployee.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsEmployee.Name = "tsEmployee";
+            this.tsEmployee.Size = new System.Drawing.Size(66, 73);
+            this.tsEmployee.Text = "Employees";
+            this.tsEmployee.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsEmployee.Click += new System.EventHandler(this.tsEmployee_Click);
+            // 
+            // tsPayroll
+            // 
+            this.tsPayroll.Enabled = false;
+            this.tsPayroll.Image = global::PayrollSytem.Properties.Resources.payrolliconC;
+            this.tsPayroll.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsPayroll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsPayroll.Name = "tsPayroll";
+            this.tsPayroll.Size = new System.Drawing.Size(66, 73);
+            this.tsPayroll.Text = "Payroll";
+            this.tsPayroll.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsPayroll.Click += new System.EventHandler(this.tsPayroll_Click);
+            // 
+            // tsUser
+            // 
+            this.tsUser.Enabled = false;
+            this.tsUser.Image = global::PayrollSytem.Properties.Resources.usersIconc;
+            this.tsUser.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsUser.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsUser.Name = "tsUser";
+            this.tsUser.Size = new System.Drawing.Size(66, 73);
+            this.tsUser.Text = "Users";
+            this.tsUser.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsUser.Click += new System.EventHandler(this.tsUser_Click);
+            // 
+            // tsReport
+            // 
+            this.tsReport.Enabled = false;
+            this.tsReport.Image = global::PayrollSytem.Properties.Resources.ReportsC;
+            this.tsReport.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsReport.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsReport.Name = "tsReport";
+            this.tsReport.Size = new System.Drawing.Size(66, 73);
+            this.tsReport.Text = "Reports";
+            this.tsReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsReport.Click += new System.EventHandler(this.tsReport_Click);
+            // 
+            // tsLogin
+            // 
+            this.tsLogin.Image = global::PayrollSytem.Properties.Resources._lock;
+            this.tsLogin.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsLogin.Name = "tsLogin";
+            this.tsLogin.Size = new System.Drawing.Size(66, 73);
+            this.tsLogin.Text = "Login";
+            this.tsLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsLogin.Click += new System.EventHandler(this.tsLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 505);
+            this.Controls.Add(this.toolStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payroll Management System - Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsEmployee;
+        private System.Windows.Forms.ToolStripButton tsPayroll;
+        private System.Windows.Forms.ToolStripButton tsUser;
+        private System.Windows.Forms.ToolStripButton tsReport;
+        private System.Windows.Forms.ToolStripButton tsLogin;
+    }
+}
+
